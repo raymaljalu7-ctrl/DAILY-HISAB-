8a05250e3961cb2e724fe9984d39898f36e9b03f
raymaljalu7-ctrl <raymaljalu7@gmail.com>
2026-09-07 02:11:10 +0530

    Add payment made totals to dashboard

--- lib/services/dashboard_service.dart
+++ lib/services/dashboard_service.dart
@@ -3,6 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class DashboardSummary {
   final double sales;
   final double paymentsReceived;
+  final double paymentsMade;
+  final double cashPaymentsMade;
+  final double bankPaymentsMade;
   final double outstanding;
   final double commission;
   final double expenses;
@@ -15,6 +18,9 @@ class DashboardSummary {
   const DashboardSummary({
     this.sales = 0,
     this.paymentsReceived = 0,
+    this.paymentsMade = 0,
+    this.cashPaymentsMade = 0,
+    this.bankPaymentsMade = 0,
     this.outstanding = 0,
     this.commission = 0,
     this.expenses = 0,
@@ -28,74 +34,37 @@ class DashboardSummary {
 
 class DashboardService {
   DashboardService._();
-
   static final DashboardService instance = DashboardService._();
-
   final FirebaseFirestore _db = FirebaseFirestore.instance;
 
-  CollectionReference<Map<String, dynamic>> _collection(
-    String name,
-  ) {
-    return _db
-        .collection('sharedData')
-        .doc('dailyHisab')
-        .collection(name);
-  }
+  CollectionReference<Map<String, dynamic>> _collection(String name) =>
+      _db.collection('sharedData').doc('dailyHisab').collection(name);
 
   DateTime _date(dynamic value) {
-    if (value is Timestamp) {
-      return value.toDate();
-    }
-
-    if (value is DateTime) {
-      return value;
-    }
-
+    if (value is Timestamp) return value.toDate();
+    if (value is DateTime) return value;
     return DateTime.fromMillisecondsSinceEpoch(0);
   }
 
-  bool _inRange(
-    DateTime date,
-    DateTime from,
-    DateTime to,
-  ) {
-    final start = DateTime(
-      from.year,
-      from.month,
-      from.day,
-    );
-
-    final end = DateTime(
-      to.year,
-      to.month,
-      to.day,
-      23,
-      59,
-      59,
-      999,
-    );
-
+  bool _inRange(DateTime date, DateTime from, DateTime to) {
+    final start = DateTime(from.year, from.month, from.day);
+    final end = DateTime(to.year, to.month, to.day, 23, 59, 59, 999);
     return !date.isBefore(start) && !date.isAfter(end);
   }
 
-  Future<DashboardSummary> calculate({
-    required DateTime from,
-    required DateTime to,
-  }) async {
+  Future<DashboardSummary> calculate({required DateTime from, required DateTime to}) async {
     final salesSnapshot = await _collection('sales').get();
-    final transactionSnapshot =
-        await _collection('transactions').get();
-    final expenseSnapshot =
-        await _collection('productionExpenses').get();
-    final salarySnapshot =
-        await _collection('salary').get();
-    final capitalSnapshot =
-        await _collection('capital').get();
-    final productionSnapshot =
-        await _collection('production').get();
+    final transactionSnapshot = await _collection('transactions').get();
+    final expenseSnapshot = await _collection('productionExpenses').get();
+    final salarySnapshot = await _collection('salary').get();
+    final capitalSnapshot = await _collection('capital').get();
+    final productionSnapshot = await _collection('production').get();
 
     double sales = 0;
     double paymentsReceived = 0;
+    double paymentsMade = 0;
+    double cashPaymentsMade = 0;
+    double bankPaymentsMade = 0;
     double outstanding = 0;
     double commission = 0;
     double expenses = 0;
@@ -106,103 +75,61 @@ class DashboardService {
 
     for (final doc in salesSnapshot.docs) {
       final data = doc.data();
-      final date = _date(data['date']);
-
-      if (!_inRange(date, from, to)) {
-        continue;
-      }
-
-      sales +=
-          (data['grossAmount'] as num?)?.toDouble() ?? 0;
-
-      paymentsReceived +=
-          (data['paymentReceived'] as num?)?.toDouble() ?? 0;
-
-      outstanding +=
-          (data['outstanding'] as num?)?.toDouble() ?? 0;
-
-      commission +=
-          (data['commission'] as num?)?.toDouble() ?? 0;
+      if (!_inRange(_date(data['date']), from, to)) continue;
+      sales += (data['grossAmount'] as num?)?.toDouble() ?? 0;
+      paymentsReceived += (data['paymentReceived'] as num?)?.toDouble() ?? 0;
+      outstanding += (data['outstanding'] as num?)?.toDouble() ?? 0;
+      commission += (data['commission'] as num?)?.toDouble() ?? 0;
     }
 
     for (final doc in transactionSnapshot.docs) {
       final data = doc.data();
-      final date = _date(data['date']);
-
-      if (!_inRange(date, from, to)) {
-        continue;
-      }
-
+      if (!_inRange(_date(data['date']), from, to)) continue;
       final type = data['type']?.toString() ?? '';
-      final amount =
-          (data['amount'] as num?)?.toDouble() ?? 0;
-
+      final amount = (data['amount'] as num?)?.toDouble() ?? 0;
+      final account = data['account']?.toString() ?? 'Cash';
       if (type == 'Receipt') {
         paymentsReceived += amount;
+      } else if (type == 'Payment') {
+        paymentsMade += amount;
+        if (account == 'Bank') {
+          bankPaymentsMade += amount;
+        } else {
+          cashPaymentsMade += amount;
+        }
       }
     }
 
     for (final doc in expenseSnapshot.docs) {
       final data = doc.data();
-      final date = _date(data['date']);
-
-      if (!_inRange(date, from, to)) {
-        continue;
-      }
-
-      expenses +=
-          (data['amount'] as num?)?.toDouble() ?? 0;
+      if (!_inRange(_date(data['date']), from, to)) continue;
+      expenses += (data['amount'] as num?)?.toDouble() ?? 0;
     }
-
     for (final doc in salarySnapshot.docs) {
       final data = doc.data();
-      final date = _date(data['date']);
-
-      if (!_inRange(date, from, to)) {
-        continue;
-      }
-
-      salary +=
-          (data['amount'] as num?)?.toDouble() ?? 0;
+      if (!_inRange(_date(data['date']), from, to)) continue;
+      salary += (data['amount'] as num?)?.toDouble() ?? 0;
     }
-
     for (final doc in capitalSnapshot.docs) {
       final data = doc.data();
-      final date = _date(data['date']);
-
-      if (!_inRange(date, from, to)) {
-        continue;
-      }
-
+      if (!_inRange(_date(data['date']), from, to)) continue;
       final type = data['type']?.toString() ?? '';
-      final amount =
-          (data['amount'] as num?)?.toDouble() ?? 0;
-
-      if (type == 'Capital Received') {
-        capitalReceived += amount;
-      } else if (type == 'Capital Returned') {
-        capitalReturned += amount;
-      }
+      final amount = (data['amount'] as num?)?.toDouble() ?? 0;
+      if (type == 'Capital Received') capitalReceived += amount;
+      if (type == 'Capital Returned') capitalReturned += amount;
     }
-
     for (final doc in productionSnapshot.docs) {
       final data = doc.data();
-      final date = _date(data['date']);
-
-      if (!_inRange(date, from, to)) {
-        continue;
-      }
-
-      productionBoxes +=
-          (data['quantity'] as num?)?.toDouble() ?? 0;
+      if (!_inRange(_date(data['date']), from, to)) continue;
+      productionBoxes += (data['quantity'] as num?)?.toDouble() ?? 0;
     }
-
-    final profitLoss =
-        sales - commission - expenses - salary;
 
     return DashboardSummary(
       sales: sales,
       paymentsReceived: paymentsReceived,
+      paymentsMade: paymentsMade,
+      cashPaymentsMade: cashPaymentsMade,
+      bankPaymentsMade: bankPaymentsMade,
       outstanding: outstanding,
       commission: commission,
       expenses: expenses,
@@ -210,7 +137,7 @@ class DashboardService {
       capitalReceived: capitalReceived,
       capitalReturned: capitalReturned,
       productionBoxes: productionBoxes,
-      profitLoss: profitLoss,
+      profitLoss: sales - commission - expenses - salary,
     );
   }
 }
